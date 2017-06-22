ad54e887875702b88804011c8b16bd8b290f3655
nestorexD <nestore156@gmail.com>
2017-06-22 20:51:06 +0900

    tercer commit

--- resources/views/cloudservice/arequipacosas/arequipacosas.blade.php
+++ resources/views/cloudservice/arequipacosas/arequipacosas.blade.php
@@ -4,11 +4,27 @@
 	<div class="politica">
 		<h1> Arequipacosas.com </h1>
 		<p> Arequipacosas es la nueva empresa que llega a su alcance para brindar un servicio de calidad, siendo un servicio innovador y atento a la vanguardia de nuestros tiempos, pone a su disposición, una galeria de negocios y publicidad para los mismos con herramientas nuevas y muy económicas, al igual que para los usuarios la garantia que podrá encontrar en un sólo sitio todo lo que necesita. Los números de contacto, nombre de empresa, rubro del negocio, la ubicación estan garantizados por la empresa, ya que es necesario para muchos saber con quien puede trabajar o contratar un servicio, recomendarlo y volverlo a encontrar.</p>
-		<p>Somos la plataforma en la que encontrarás todos los negocios de Arequipa, con la garantía que el negocio ha sido visitado y orientado a brindarte a ti un servicio adicional al acostumbrado, la calidad del servicio será medido por los usuarios</p>
+		<p>Somos la plataforma en la que encontrarás todos los negocios de Arequipa, con la garantía que el negocio ha sido visitado y orientado a brindarte a ti un servicio adicional al acostumbrado, la calidad del servicio será medido por los usuarios</p><br>
 
 		<h2>Nuestra Misión</h2>
 		<p>Para los negocios, brindar una plataforma web para la publicación de su negocio; en el cual, el establecimiento podrá anunciar con sus banners informativos, imagenes referenciales sus productos y servicios de calidad, la ubicación y sus números para contactarlos, etc.</p>
-		<p>Para los usuarios, brindar una plataforma de búsqueda para que encuentren el servicio que necesiten. Un negocio que se ajuste a su tiempo, ya que en la Empresa Arequipacosas, entendemos que el tiempo es dinero.</p>
+		<p>Para los usuarios, brindar una plataforma de búsqueda para que encuentren el servicio que necesiten. Un negocio que se ajuste a su tiempo, ya que en la Empresa Arequipacosas, entendemos que el tiempo es dinero.</p><br>
+	
+		<h3>Ventajas para los negocios:</h3>
+		<ul>
+			<p><li>Único cobro por publicación, realmente económico. Garantia y pago anual. <br>Para mas informacion comunicarse al: 924873809 o 959339170 </li><br>
+			<li>Proveer a los negocios de una Plantilla Web para que anuncien y publiquen su negocio. (Informacion e imagenes)</li><br>
+			<li>Arequipacosas NO cobra comisión por las ventas que puedan realizar el negocio.</li><br>
+			<li>Mayor facilidad de poder ser RECOMENDADO. Ya que muchos se olvidan del nombre del negocio, pero se puede volver a encontrar en la plataforma de busqueda Arequipacosas.</li><br>
+			<li>Asesoramiento digital a los negocios en información web y enseñanza personalizada.</li></p><br>
+		</ul>
+		<h3>Ventajas para los usuarios:</h3>
+		<ul>
+			<p><li>Poder navegar por los negocios y servicios de Arequipa.</li><br>
+			<li>Comunicarse inmediatamente con el establecimiento y establecer las necesidades de compra y volver a encontrarlo si lo requiere.</li><br>
+			<li>Poder recomendar un negocio por el buen trato, o por satisfacción del cliente.</li><br>
+			<li>Poder encontrar el servicio que necesito en un menor tiempo que el habitual.</li></p>
+		</ul>
 	</div>
 
 @endsection
